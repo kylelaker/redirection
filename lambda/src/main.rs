5c85244420a42a4b3f8bae5142e0f909541d9b84
Kyle Laker <kyle@laker.email>
2021-12-11 20:54:35 -0500

    Refactor handler

--- lambda/src/main.rs
+++ lambda/src/main.rs
@@ -26,17 +26,20 @@ struct RedirectGetErrorResponse {
 
 #[derive(Debug, Error)]
 enum RedirectGetError {
-    #[error("host was not found")]
+    #[error("No redirect location found for the given host")]
     HostNotFound,
-    #[error("location format is invalid")]
-    InvalidFormat,
-    #[error("request is invalid")]
+    #[error("The request was invalid")]
     BadRequest,
+    #[error("The redirect record is invalid")]
+    InvalidFormat,
+    #[error("An unknown error condition occurred")]
+    UnknownError,
 }
 
 impl IntoResponse for RedirectGetError {
     fn into_response(self) -> Response<Body> {
         let status = match self {
+            RedirectGetError::BadRequest => 400,
             RedirectGetError::HostNotFound => 404,
             _ => 500,
         };
@@ -46,58 +49,93 @@ impl IntoResponse for RedirectGetError {
         };
         Response::builder()
             .status(status)
+            .header("Content-Type", "application/json")
             .body(Body::from(serde_json::to_string(&response).unwrap()))
             .expect("failed to render result")
     }
 }
 
-fn get_item_to_response(output: GetItemOutput) -> Response<Body> {
+impl IntoResponse for RedirectGetResponse {
+    fn into_response(self) -> Response<Body> {
+        Response::builder()
+            .status(301)
+            .header("Location", self.redirect.location)
+            .body(Body::Empty)
+            .expect("faied to render response")
+    }
+}
+
+fn get_item_to_response(
+    host: &str,
+    output: GetItemOutput,
+) -> Result<RedirectGetResponse, RedirectGetError> {
     let record = if let Some(item) = output.item {
         item
     } else {
-        return RedirectGetError::HostNotFound.into_response();
-    };
-    let location_attr = record.get("location");
-    let location_raw = if let Some(location_value) = location_attr {
-        location_value.as_s()
-    } else {
-        return RedirectGetError::HostNotFound.into_response();
+        return Err(RedirectGetError::HostNotFound);
     };
 
-    match location_raw {
-        Ok(location) => Response::builder()
-            .status(301)
-            .header("Location", location)
-            .body(Body::Empty)
-            .expect("failed to render result"),
-        Err(_) => RedirectGetError::InvalidFormat.into_response(),
+    let location_raw = if let Some(location_value) = record.get("location") {
+        location_value.as_s()
+    } else {
+        return Err(RedirectGetError::HostNotFound);
+    };
+
+    if let Ok(location) = location_raw {
+        Ok(RedirectGetResponse {
+            redirect: Redirect {
+                host: host.to_string(),
+                location: location.to_string(),
+            },
+        })
+    } else {
+        Err(RedirectGetError::InvalidFormat)
     }
+}
+
+async fn perform_query(client: Client, host: &str) -> Result<GetItemOutput, RedirectGetError> {
+    client
+        .get_item()
+        .table_name(std::env::var("REDIRECT_TABLE").unwrap())
+        .key("host", AttributeValue::S(host.to_string()))
+        .send()
+        .await
+        .map_err(|_| RedirectGetError::UnknownError)
+}
+
+fn get_request_header(event: Request, header: &str) -> Result<String, RedirectGetError> {
+    event.headers().get(header).map_or_else(
+        || Err(RedirectGetError::BadRequest),
+        |v| {
+            v.to_str()
+                .map(|op| op.to_string())
+                .map_err(|_| RedirectGetError::BadRequest)
+        },
+    )
+}
+
+async fn func(event: Request, _: Context) -> Result<impl IntoResponse, LambdaError> {
+    let shared_config = aws_config::load_from_env().await;
+    let client = Client::new(&shared_config);
+
+    let host = match get_request_header(event, "Host") {
+        Ok(header) => header,
+        Err(e) => return Ok(e.into_response()),
+    };
+
+    let output = match perform_query(client, host.as_str()).await {
+        Ok(output) => output,
+        Err(e) => return Ok(e.into_response()),
+    };
+
+    Ok(match get_item_to_response(host.as_str(), output) {
+        Ok(response) => response.into_response(),
+        Err(e) => e.into_response(),
+    })
 }
 
 #[tokio::main]
 async fn main() -> Result<(), LambdaError> {
     lambda_runtime::run(handler(func)).await?;
     Ok(())
-}
-
-async fn func(event: Request, _: Context) -> Result<impl IntoResponse, LambdaError> {
-    let shared_config = aws_config::load_from_env().await;
-    let client = Client::new(&shared_config);
-    let host_header = event.headers().get("Host").unwrap().to_str();
-    println!("Looking up: {:?}", host_header);
-    let host = if let Ok(header) = host_header {
-        header.to_string()
-    } else {
-        return Ok(RedirectGetError::BadRequest.into_response());
-    };
-
-    client
-        .get_item()
-        .table_name(std::env::var("REDIRECT_TABLE").unwrap())
-        .key("host", AttributeValue::S(host))
-        .send()
-        .await
-        .map(get_item_to_response)
-        .map_err(|_| RedirectGetError::HostNotFound)
-        .or_else(|e| Ok(e.into_response()))
 }
